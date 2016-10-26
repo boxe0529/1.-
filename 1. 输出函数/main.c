2af54ccfa8984db4c899c7e66ae060d446ff073a
boxe0529 <34657719@qq.com>
2016-10-26 21:16:32 +0800

    input

---
+++ 1. 输出函数/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  1. 输出函数
+//
+//  Created by 邓云方 on 15/7/21.
+//  Copyright (c) 2015年 邓云方. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("欢迎来到我赢职场阶段考核，我正在考试，请勿骚扰！！\n");
+    return 0;
+}
